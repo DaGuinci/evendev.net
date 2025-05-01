0c41768dce5bd1ddd09d5618af326cda5d9dd990
Guillaume Even <daguinci@mailo.com>
2025-05-01 11:23:24 +0200

    dev(): setup sections and routes in nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './sections/about/about.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent },
+];
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [
+    RouterLink,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
