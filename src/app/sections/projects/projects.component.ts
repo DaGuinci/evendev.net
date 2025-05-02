ade52f2b9e90976bc80a7abfbb582d9b3c582b84
Guillaume Even <daguinci@mailo.com>
2025-05-03 01:08:32 +0200

    dev(): projects content in process

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+// import { MatButtonModule } from '@angular/material/button';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'projects'
+  }
 })
 export class ProjectsComponent {
 
