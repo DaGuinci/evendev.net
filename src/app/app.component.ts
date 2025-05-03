d4d3133fce8d0aba71136ab46b95e542bf12c1b4
Guillaume Even <daguinci@mailo.com>
2025-05-03 13:02:32 +0200

    styles(): background animated changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router'; // Import du service Router
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   imports: [
     NgClass,
+    NgStyle,
     RouterOutlet,
     NavComponent
   ],
@@ -17,23 +18,37 @@ import { NgClass } from '@angular/common';
 })
 export class AppComponent {
   title = 'evendev.net';
-
+  
   globaleSectionActive: string = '';
   isExiting: boolean = false;
+  bgExiting: boolean = false;
+  gradient = 'radial-gradient(circle,rgba(0, 0, 0, .4) 0%, rgba(0, 0, 0, 1) 80%)';
+  url = '/img/home-bg.jpg';
+  backgroundImage = `${this.gradient}, url('${this.url}')`;
 
-  constructor(private router: Router) {} // Injection du service Router
+
+  constructor(private router: Router) {}
 
   onGlobalSectionActive(section: string) {
+    // from section to section and section to home
     if (this.globaleSectionActive && this.globaleSectionActive !== section) {
       this.isExiting = true;
+      this.bgExiting = true;
       setTimeout(() => {
         this.isExiting = false;
+        this.updateBackgroundImage(section);
         this.globaleSectionActive = section;
-        this.router.navigate([`/${section}`]); // Navigation après le délai
-      }, 500); // Durée de l'animation
+        this.router.navigate([`/${section}`]);
+      }, 500);
+    // from home to section
     } else {
+      this.bgExiting = true;
       this.globaleSectionActive = section;
-      this.router.navigate([`/${section}`]); // Navigation immédiate
+      setTimeout(() => {
+        // this.bgExiting = false;
+        this.updateBackgroundImage(section);
+      }, 500);
+      this.router.navigate([`/${section}`]);
     }
   }
 
@@ -42,7 +57,33 @@ export class AppComponent {
     this.globaleSectionActive = '';
     setTimeout(() => {
       this.isExiting = false;
-      this.router.navigate(['/']); // Navigation vers la page d'accueil après le délai
-    }, 500); // Durée de l'animation
+      this.router.navigate(['/']);
+    }, 500);
+  }
+
+  updateBackgroundImage(section: string) {
+    switch (section) {
+      case 'projects':
+        // this.backgroundImage = `${this.gradient}, url('/img/projects-bg.jpg')`;
+        this.url = `/img/projects-bg.jpg`;
+        console.log(this.url);
+        break;
+      case 'about':
+        this.url = '/img/about-bg.jpg';
+        console.log(this.url);
+        break;
+      case 'contact':
+        this.url = '/img/contact-bg.jpg';
+        console.log(this.url);
+        break;
+      default:
+        this.url = '/img/home-bg.jpg';
+        console.log(this.url);
+        break;
+    }
+    this.backgroundImage = `${this.gradient}, url('${this.url}')`;
+    setTimeout(() => {
+      this.bgExiting = false;
+    }, 200);
   }
 }
