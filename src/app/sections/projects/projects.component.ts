b1193cfc6e61bfc7b76a2e6ed2acc70b4757c900
Guillaume Even <daguinci@mailo.com>
2025-05-06 23:18:31 +0200

    refactor: remove unused Angular Material dependencies and update styles

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { ChangeDetectionStrategy } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-// import { MatButtonModule } from '@angular/material/button';
 import { ViewEncapsulation } from '@angular/core';
 
 @Component({
